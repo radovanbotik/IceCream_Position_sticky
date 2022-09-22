adc6e3880316279793d2fcc06afb78d752e53eb4
radovanbotik <radovanbotik@gmail.com>
2022-09-22 10:17:30 +0200

    'dummy data'

---
+++ products.js
@@ -0,0 +1,29 @@
+const products = [
+  {
+    id: "chocolate dream",
+    image:
+      "./assets/images/products/charlota-blunarova-rUGiNjP4OX4-unsplash.jpg",
+  },
+  {
+    id: "cinnamon punch",
+    image:
+      "./assets/images/products/charlota-blunarova-rUGiNjP4OX4-unsplash.jpg",
+  },
+  {
+    id: "berry fusion",
+    image:
+      "./assets/images/products/charlota-blunarova-rUGiNjP4OX4-unsplash.jpg",
+  },
+  {
+    id: "coffee overdose",
+    image:
+      "./assets/images/products/charlota-blunarova-rUGiNjP4OX4-unsplash.jpg",
+  },
+  {
+    id: "tropic sorbet",
+    image:
+      "./assets/images/products/charlota-blunarova-rUGiNjP4OX4-unsplash.jpg",
+  },
+];
+
+export { products };
---
+++ recipes.js
@@ -0,0 +1,129 @@
+const recipes = [
+  {
+    id: "chocolate dream",
+    ingredients: [
+      {
+        ingredient: "chocolate",
+        image: "./assets/assets/images/ingredients/chocolate.jpg",
+      },
+      {
+        ingredient: "hazelnut",
+        image: "./assets/assets/images/ingredients/hazelnut.jpg",
+      },
+      {
+        ingredient: "milk",
+        image: "./assets/assets/images/ingredients/milk.jpg",
+      },
+      {
+        ingredient: "sugar",
+        image: "./assets/assets/images/ingredients/sugar.jpg",
+      },
+      {
+        ingredient: "eggs",
+        image: "./assets/assets/images/ingredients/eggs-two.jpg",
+      },
+    ],
+  },
+  {
+    id: "cinnamon punch",
+    ingredients: [
+      {
+        ingredient: "cinnamon",
+        image: "./assets/assets/images/ingredients/cinnamon-sec.jpg",
+      },
+      {
+        ingredient: "almonds",
+        image: "./assets/assets/images/ingredients/almonds.jpg",
+      },
+      {
+        ingredient: "milk",
+        image: "./assets/assets/images/ingredients/milk.jpg",
+      },
+      {
+        ingredient: "sugar",
+        image: "./assets/assets/images/ingredients/sugar.jpg",
+      },
+      {
+        ingredient: "eggs",
+        image: "./assets/assets/images/ingredients/eggs-two.jpg",
+      },
+    ],
+  },
+  {
+    id: "berry fusion",
+    ingredients: [
+      {
+        ingredient: "strawberry",
+        image: "./assets/assets/images/ingredients/strawberry.jpg",
+      },
+      {
+        ingredient: "banana",
+        image: "./assets/assets/images/ingredients/banana.jpg",
+      },
+      {
+        ingredient: "milk",
+        image: "./assets/assets/images/ingredients/milk.jpg",
+      },
+      {
+        ingredient: "sugar",
+        image: "./assets/assets/images/ingredients/sugar.jpg",
+      },
+      {
+        ingredient: "eggs",
+        image: "./assets/assets/images/ingredients/eggs-two.jpg",
+      },
+    ],
+  },
+  {
+    id: "coffee overdose",
+    ingredients: [
+      {
+        ingredient: "roasted coffee grains",
+        image: "./assets/assets/images/ingredients/coffee-grains.jpg",
+      },
+      {
+        ingredient: "espresso",
+        image: "./assets/assets/images/ingredients/coffee-cup.jpg",
+      },
+      {
+        ingredient: "milk",
+        image: "./assets/assets/images/ingredients/milk.jpg",
+      },
+      {
+        ingredient: "sugar",
+        image: "./assets/assets/images/ingredients/sugar.jpg",
+      },
+      {
+        ingredient: "eggs",
+        image: "./assets/assets/images/ingredients/eggs-two.jpg",
+      },
+    ],
+  },
+  {
+    id: "tropic sorbet",
+    ingredients: [
+      {
+        ingredient: "banana",
+        image: "./assets/assets/images/ingredients/banana.jpg",
+      },
+      {
+        ingredient: "kiwi-sec",
+        image: "./assets/assets/images/ingredients/kiwi-sec.jpg",
+      },
+      {
+        ingredient: "chunks of dried pineapple",
+        image: "./assets/assets/images/ingredients/dried-pineapple.jpg",
+      },
+      {
+        ingredient: "mint",
+        image: "./assets/assets/images/ingredients/mint.jpg",
+      },
+      {
+        ingredient: "milk",
+        image: "./assets/assets/images/ingredients/milk.jpg",
+      },
+    ],
+  },
+];
+
+export { recipes };
