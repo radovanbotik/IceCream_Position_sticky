c9bbc8c1aae3ce3b8cabb22dceaed6e214bbccc3
radovanbotik <radovanbotik@gmail.com>
2022-09-22 16:27:13 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-import { products } from "./products.js";
-import { recipes } from "./recipes.js";
 import { getElement } from "./getElement.js";
 import { observeProducts } from "./observeProducts.js";
-
-const productViewRight = getElement(".product-view-right");
-
-const renderProducts = () => {
-  const productsHTML = products
-    .map(product => {
-      const { id, image } = product;
-      return `
-    <article id="${id}">
-      <img
-         src="${image}"
-         alt="portrait of product ${id}"
-         />
-    </article>
-    `;
-    })
-    .join("");
-  productViewRight.innerHTML = productsHTML;
-  return productViewRight;
-};
+import { renderProducts } from "./renderProducts.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   renderProducts();
