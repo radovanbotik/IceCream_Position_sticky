04c811aabcaa258bae670cf8af664b8b0312d0f0
radovanbotik <radovanbotik@gmail.com>
2022-09-23 23:30:45 +0200

    please release me from my empty shell

---
+++ headerHandler.js
@@ -0,0 +1,34 @@
+import { getElement } from "./getElement.js";
+const header = getElement("header");
+
+const headerHandler = () => {
+  const options = {
+    threshold: 1,
+  };
+  const callback = (entries, observer) => {
+    entries.forEach(entry => {
+      console.log(entry);
+
+      if (entry.boundingClientRect.y === 0) {
+        document.querySelector(".navbar").classList.add("swap-colors");
+      } else {
+        document.querySelector(".navbar").classList.remove("swap-colors");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, options);
+  observer.observe(document.querySelector(".left"));
+
+  let previousScroll = 0;
+  window.addEventListener("scroll", () => {
+    let currentScroll = window.pageYOffset;
+    if (previousScroll < currentScroll) {
+      header.classList.add("logo-sizedown");
+    } else {
+      header.classList.remove("logo-sizedown");
+    }
+    currentScroll = previousScroll;
+  });
+};
+
+export { headerHandler };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { getElement } from "./getElement.js";
 import { observeProducts } from "./observeProducts.js";
 import { renderProducts } from "./renderProducts.js";
-import { observeLogo } from "./observeLogo.js";
+import { headerHandler } from "./headerHandler.js";
 
 window.addEventListener("DOMContentLoaded", () => {
-  observeLogo();
+  headerHandler();
   const renderedProducts = renderProducts();
   observeProducts(renderedProducts);
 });
