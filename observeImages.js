233bbf6199a1190a2e069e9511be30947e157b01
radovanbotik <radovanbotik@gmail.com>
2022-09-25 19:06:27 +0200

    ''

--- observeImages.js
+++ observeImages.js
@@ -4,15 +4,14 @@ import { renderIngredients } from "./renderIngredients.js";
 const observeImages = productsRight => {
   const products = [...productsRight.querySelectorAll("article")];
   const options = {
-    threshold: 0.6,
+    threshold: 0.5,
   };
 
   const productsCallback = entries => {
     entries.map(entry => {
-      console.log(entry.target);
       if (entry.isIntersecting) {
         renderIngredients(entry.target);
-
+        console.log(entry);
         entry.target.style.opacity = 1;
         return entry.target;
       } else {
